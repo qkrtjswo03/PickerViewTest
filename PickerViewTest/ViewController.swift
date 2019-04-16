21ef36cb7e932ddf51d27d941ebc636ed03002c1
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-04-16 15:39:23 +0900

    pickerView Test

--- PickerViewTest/ViewController.swift
+++ PickerViewTest/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    var myColor = ["red", "green", "blue"]
+    @IBOutlet weak var outLabel: UILabel!
+    @IBOutlet weak var myPickerView: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Deleagte 객체 연결
+        myPickerView.delegate = self
+        myPickerView.dataSource = self
+        
+        // 실행 초기 Label 문자를 "red"로 변경
+        outLabel.text = myColor[0]
     }
-
-
+    
+    // UIPickerViewDataSource 메소드 호출
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return myColor.count
+    }
+    
+    // UIPickerViewDelegate 메소드 호출
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return myColor[row]
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        outLabel.text = myColor[row]
+    }
 }
 
